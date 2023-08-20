9aeed8f7f697df1ff424db09ce077d0ed32c03fe
Arseniy Lebedev <Lordkrab@bk.ru>
2023-08-20 22:19:48 +0300

    task 1 has been done

--- apihomework.py
+++ apihomework.py
@@ -1,2 +1,14 @@
 import requests
 
+url = 'https://akabab.github.io/superhero-api/api/all.json'
+response = requests.get(url)
+names_of_characters = ["Hulk", "Captain America", "Thanos"]
+intelligence = {}
+
+if response.status_code == 200:
+    for character in response.json():
+        if character['name'] in names_of_characters:
+            intelligence[character['name']] = character['powerstats']['intelligence']
+    print(max(intelligence, key=intelligence.get))
+else:
+    print("Ошибочка вышла")
